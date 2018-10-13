652edc9b6757a7aec7a0141047096f101b0942bf
dbranco <gdbranco@gmail.com>
2018-10-13 18:37:54 -0300

    Working example of data binding and Input Ouput Communication through event emitters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  curComponent = "Recipe";
+  onNavigate(component: string){
+    this.curComponent = component;
+  }
 }
--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,15 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Test recipe', 'testing out', 'https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?cs=srgb&dl=food-dinner-lunch-70497.jpg&fm=jpg')
+    new Recipe("Another recipe", "another test","http://aispantherexpress.com/wp-content/uploads/2017/04/food-choices.jpg")
   ];
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
   constructor() { }
 
   ngOnInit() {
