27c51604917e8d3501de81fb705eefacbe77aa0c
dbranco <gdbranco@gmail.com>
2018-10-14 20:39:19 -0300

    Added popper.js for dropdown in bootstrap@4. TODO: Services + routing

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Test recipe', 'testing out', 'https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?cs=srgb&dl=food-dinner-lunch-70497.jpg&fm=jpg')
+    new Recipe('Test recipe', 'testing out', 'https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?cs=srgb&dl=food-dinner-lunch-70497.jpg&fm=jpg'),
     new Recipe("Another recipe", "another test","http://aispantherexpress.com/wp-content/uploads/2017/04/food-choices.jpg")
   ];
 
